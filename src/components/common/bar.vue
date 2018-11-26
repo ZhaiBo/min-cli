0b16670c2c08e85d4dd12dafaf72ae487b95b423
zhaibo <729963917@qq.com>
2018-11-26 08:48:36 +0800

    add sass

---
+++ src/components/common/bar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bar">
+    <p>h哈和</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: 'bar'
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+  $primary-color: #333
+
+  .bar
+    width: 100px
+    height: 100 / 2 px
+    background: $primary-color
+    p
+      color: red
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,18 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+import Bar from '../components/common/bar'
+
 export default new Router({
   routes: [
+    {
+      path: '/',
+      redirect:'/bar'
+    },
+    {
+      name: 'bar',
+      path: '/bar',
+      component: Bar
+    }
   ]
 })
